79323d66d3c0940f96424979df6c5689957e15e8
PAULO-DEV <paulo.maculan@gmail.com>
2018-02-28 22:05:56 -0300

    Injected dependency of IConfiguration into Greeter class and get the Greeting key value from the system appsettings.json file.

---
+++ OdeToFood/IGreeter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace OdeToFood
+{
+    public interface IGreeter
+    {
+        string GetMessageOfTheDay();
+    }
+
+    public class Gretter : IGreeter
+    {
+        private IConfiguration _configuration;
+
+        public Gretter(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string GetMessageOfTheDay()
+        {
+            return _configuration["Greeting"];
+        }
+    }
+}
