b470d9eafba7ee39245f73c3a8a45e9fc8955f60
PAULO-DEV <paulo.maculan@gmail.com>
2018-03-01 15:28:41 -0300

    Attribute Routes

--- OdeToFood/Controllers/AboutController.cs
+++ OdeToFood/Controllers/AboutController.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace OdeToFood.Controllers
 {
+    [Route("[controller]")]
     public class AboutController
     {
+        [Route("")]
         public string Phone()
         {
             return "+1+555+555+5555";
         }
 
+        [Route("[action]")]
         public string Address()
         {
             return "USA";
